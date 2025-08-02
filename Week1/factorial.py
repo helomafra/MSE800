baa9959203b447cce3ce3dc761e5315bc78ac2d8
Helô <heloisacmafra@gmail.com>
2025-08-02 15:02:01 +1200

    Week1(feat): add get_valid_input() function to factorial

--- Week1/factorial.py
+++ Week1/factorial.py
@@ -1,21 +1,27 @@
 def factorial(number):
   result = 1
-  if number < 0: 
-    print("Error: number must be positive!")
-    return
-  elif number == 0: 
-    print("Factorial of 0 is 1")
+  if number == 0: 
     return result
-  elif number == 1:
-    print("Factorial of 1 is 1")
+  if number == 1:
     return result
   else: 
     for i in range(1, number + 1):
       result = result * i
     return result
 
+def get_valid_input():
+  while True:
+    try:
+      number = int(input("Enter a number: "))
+      if number < 0:
+        print("Error: number must be positive!")
+        continue
+      return number
+    except ValueError:
+      print("Error: Invalid input. Please enter a valid integer number.")
+
 if __name__ == "__main__":
-  number = input("Enter a number:")
-  number = int(number)
+  number = get_valid_input()
   result = factorial(number)
-  print(f"Factorial of {number} is {result}")
+  if result is not None:
+    print(f"Factorial of {number} is {result}")
