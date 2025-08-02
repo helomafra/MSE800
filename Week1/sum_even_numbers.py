4a87a8afb8542b71953406db1b04a4420a975a86
Helô <heloisacmafra@gmail.com>
2025-08-02 15:27:23 +1200

    Week1(feat&refactor): add sum of odd numbers and while loop

--- Week1/sum_even_numbers.py
+++ Week1/sum_even_numbers.py
@@ -14,17 +14,30 @@ def get_valid_input():
       return number
     except ValueError:
       print("Error: Invalid input. Please enter a valid integer number.")
- 
+
 def get_sum_of_even_numbers(number):
   sum = 0
-  for i in range(1, number + 1):
+  i = 1
+  while (i <= number):
     if i % 2 == 0:
       sum += i
       print(f"Even number: {i}")
+    i += 1
   return sum
 
+def get_sum_of_odd_numbers(number):
+  sum = 0
+  i = 1
+  while (i <= number):
+    if i % 2 != 0:
+      sum += i
+      print(f"Odd number: {i}")
+    i += 1
+  return sum
 
 if __name__ == "__main__":
   number = get_valid_input()
-  result_event = get_sum_of_even_numbers(number)
-  print(f"The sum of even numbers between 1 and {number} is {result_event}")
+  result_even = get_sum_of_even_numbers(number)
+  result_odd = get_sum_of_odd_numbers(number)
+  print(f"The sum of even numbers between 1 and {number} is {result_even}")
+  print(f"The sum of odd numbers between 1 and {number} is {result_odd}")
