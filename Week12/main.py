161663afff99f38824ed418f0303c17c59bcf193
Helô <heloisacmafra@gmail.com>
2025-10-18 11:36:50 +1300

    feat(flask): add new paths

--- Week12/main.py
+++ Week12/main.py
@@ -10,6 +10,14 @@ app = Flask(__name__)
 def hello_world():
     return '<h1>Hello, World!</h1><p>Welcome to your first Flask application!</p>'
 
+@app.route('/username/<name>')
+def hello_name(name):
+    return f'<h1>Hello, {name}!</h1><p>Nice to meet you!</p>'
+
+@app.route('/cal/<int:number>')
+def show_square(number):
+    return f'<h1>Calculation</h1><p>The square of {number} is {number**2}</p>'
+
 # Run the application
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5100) 
